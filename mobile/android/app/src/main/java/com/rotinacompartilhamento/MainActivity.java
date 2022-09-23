6c53dbb87422b28a3718d379c259f02dac0e29bb
Isabela Moraes <isabelagmoraes@hotmail.com>
2022-09-22 23:13:31 -0300

    feat: android splash screen

--- mobile/android/app/src/main/java/com/rotinacompartilhamento/MainActivity.java
+++ mobile/android/app/src/main/java/com/rotinacompartilhamento/MainActivity.java
@@ -3,6 +3,8 @@ package com.rotinacompartilhamento;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import org.devio.rn.splashscreen.SplashScreen; 
+import android.os.Bundle; 
 
 public class MainActivity extends ReactActivity {
 
@@ -36,5 +38,11 @@ public class MainActivity extends ReactActivity {
       reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
       return reactRootView;
     }
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
   }
 }
--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import { ThemeProvider } from 'styled-components';
 import Routes from './routes';
 import theme from './styles/theme';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar
