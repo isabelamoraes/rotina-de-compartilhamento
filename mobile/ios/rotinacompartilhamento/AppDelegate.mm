9d60672d95ad8ef11bf5c04bf055919650e1892c
user225754 <isabelagmoraes@hotmail.com>
2022-09-25 00:00:31 -0400

    feat: setup ios and firebase config

--- mobile/ios/rotinacompartilhamento/AppDelegate.mm
+++ mobile/ios/rotinacompartilhamento/AppDelegate.mm
@@ -1,5 +1,7 @@
 #import "AppDelegate.h"
 
+#import <Firebase.h>
+
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -32,7 +34,9 @@
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-
+  
+  [FIRApp configure];
+  
 #if RCT_NEW_ARCH_ENABLED
   _contextContainer = std::make_shared<facebook::react::ContextContainer const>();
   _reactNativeConfig = std::make_shared<facebook::react::EmptyReactNativeConfig const>();
